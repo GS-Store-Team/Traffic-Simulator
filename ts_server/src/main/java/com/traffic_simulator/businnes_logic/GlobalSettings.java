b2d5007b124102834eb230d44f6705339efc1891
inikta <iniktus@mail.ru>
2022-12-13 00:55:18 +0700

    Замерджил business_logic в main.

--- ts_server/src/main/java/com/traffic_simulator/businnes_logic/GlobalSettings.java
+++ ts_server/src/main/java/com/traffic_simulator/businnes_logic/GlobalSettings.java
@@ -1,14 +1,11 @@
 package com.traffic_simulator.businnes_logic;
 
+import lombok.Data;
+
+@Data
 public class GlobalSettings {
-    public static final int cellWidth = 4;
-    public static final int cellLength = 1;
 
-    public static final double cellNaturalWeightModifier = 1.0;
-    public static final double cellTrafficWeightModifier = 1.0;
-
-
-    public static final int defaultCarLength = 5;
-    public static final int automobileLength = 5;
-    public static final int truckLength = 9;
+    public static int cellWidth = 4;
+    public static int cellLength = 1;
+    public static double cellTrafficWeightModifier = 1;
 }
