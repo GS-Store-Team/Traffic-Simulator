bbd2c16cb846b5a87acf77981fb6b24bdc24d032
inikta <iniktus@mail.ru>
2022-12-12 21:13:47 +0700

    Заготовка SimulationRunner.

--- ts_server/src/main/java/com/traffic_simulator/businnes_logic/simulation_runner/SimulationRunner.java
+++ ts_server/src/main/java/com/traffic_simulator/businnes_logic/simulation_runner/SimulationRunner.java
@@ -1,4 +1,23 @@
 package com.traffic_simulator.businnes_logic.simulation_runner;
 
+import com.traffic_simulator.businnes_logic.beans.SimulationContext;
+import com.traffic_simulator.dto.MapDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Lazy
 public class SimulationRunner {
+    private final SimulationContext simulationContext;
+
+    public void start() {
+
+    }
+
+    public MapDTO getCurrentState() {
+
+        return null;
+    }
 }
